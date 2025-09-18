b47a648a6be8c433c9fd56221c2a5b39f43b20ac
nguyenthequang0802 <quangnguyenthe7@gmail.com>
2025-09-18 13:34:56 +0700

    create project

---
+++ src/test/java/org/example/finaltest/FinalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
